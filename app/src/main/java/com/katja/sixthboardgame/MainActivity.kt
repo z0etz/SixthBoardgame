5dc1fee224574e7201fb88a408bd322223b6ceb4
Lindahl124 <lmatilda@live.se>
2024-05-02 23:47:30 +0200

    Added func for setOnClickListener to MainActivity.kt.

--- app/src/main/java/com/katja/sixthboardgame/MainActivity.kt
+++ app/src/main/java/com/katja/sixthboardgame/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.katja.sixthboardgame
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textButtonSignUp = findViewById<TextView>(R.id.textButtonSignUp)
+        textButtonSignUp.setOnClickListener {
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
