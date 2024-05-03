bf78b7ad610143f4d7d2fc4166c6f8c38d16341a
Lindahl124 <lmatilda@live.se>
2024-05-03 11:28:40 +0200

    Added activity_win_dialog.xml and WinDialogActivity.kt.

--- app/src/main/java/com/katja/sixthboardgame/WinDialogActivity.kt
+++ app/src/main/java/com/katja/sixthboardgame/WinDialogActivity.kt
@@ -3,9 +3,9 @@ package com.katja.sixthboardgame
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class LoginActivity : AppCompatActivity() {
+class WinDialogActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_win_dialog)
     }
 }
