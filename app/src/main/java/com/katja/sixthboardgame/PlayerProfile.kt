9ac8f0d712af23000f48e5daaf08405fbff8043a
Lindahl124 <lmatilda@live.se>
2024-05-06 13:40:29 +0200

    Added binding.textButtonSignOut to activity_player_profile.xml.

--- app/src/main/java/com/katja/sixthboardgame/PlayerProfile.kt
+++ app/src/main/java/com/katja/sixthboardgame/PlayerProfile.kt
@@ -1,11 +1,23 @@
 package com.katja.sixthboardgame
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.katja.sixthboardgame.databinding.ActivityPlayerProfileBinding
 
 class PlayerProfile : AppCompatActivity() {
+
+    lateinit var binding: ActivityPlayerProfileBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_player_profile)
+        binding = ActivityPlayerProfileBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.textButtonSignOut.setOnClickListener{
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+
+        }
     }
 }
