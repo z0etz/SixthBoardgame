b92a51a9b823e0d8bc5db37bf60b585e97831eff
jammingbadger <146725115+jammingbadger@users.noreply.github.com>
2024-05-06 11:24:59 +0200

    Function game list

--- app/src/main/java/com/katja/sixthboardgame/WelcomeActivity.kt
+++ app/src/main/java/com/katja/sixthboardgame/WelcomeActivity.kt
@@ -2,10 +2,23 @@ package com.katja.sixthboardgame
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class WelcomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
+
+        val ongoingGamesData: List<String> = getOngoingGamesData()
+        val adapter = WelcomeAdapterCurrentGamesList(this, getOngoingGamesData)
+
+        val recyclerView: RecyclerView = findViewById(R.id.recyclerViewOngoingGames)
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
+    }
+
+    private fun getOngoingGamesData(): List<String> {
+        return listOf("Game 1", "Game 2", "Game 3")
     }
 }
