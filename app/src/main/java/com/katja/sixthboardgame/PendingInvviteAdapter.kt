76e52f7ddf85a11754789dc4fddfee61eb9d4de8
Moa <moa.lovas@gmail.com>
2024-05-06 14:54:28 +0200

    Recycleview Adapter setup. With Lucas.

---
+++ app/src/main/java/com/katja/sixthboardgame/PendingInvviteAdapter.kt
@@ -0,0 +1,15 @@
+package com.katja.sixthboardgame
+
+import android.view.View
+import androidx.appcompat.view.menu.MenuView.ItemView
+import androidx.cardview.widget.CardView
+import androidx.recyclerview.widget.RecyclerView
+import com.katja.sixthboardgame.databinding.PendingInvitesBinding
+
+
+//class PendingInviteAdapter (val userList: List<User> ): RecyclerView.Adapter<UserAdapter.ViewHolder> {
+//    class ViewHolder(binding: PendingInvitesBinding) : RecyclerView.ViewHolder(binding.root) {
+//        val userName : CardView = binding.inviteCardview
+//        val recyclerView : RecyclerView = binding.in
+//    }
+//}
