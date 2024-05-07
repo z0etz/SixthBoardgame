f715bbda8fe25de7b9e8a893909be4a6c84bfebd
luma <lucas.martinahlm@gmail.com>
2024-05-07 14:46:31 +0200

    Feature delete account fixed

--- app/src/main/java/com/katja/sixthboardgame/PlayerProfileActivity.kt
+++ app/src/main/java/com/katja/sixthboardgame/PlayerProfileActivity.kt
@@ -1,11 +1,42 @@
 package com.katja.sixthboardgame
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.katja.sixthboardgame.databinding.ActivityPlayerProfileBinding
 
-class PlayerProfileActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_player_profile)
+
+    class PlayerProfileActivity : AppCompatActivity() {
+
+        lateinit var auth: FirebaseAuth
+        lateinit var binding: ActivityPlayerProfileBinding
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            binding = ActivityPlayerProfileBinding.inflate(layoutInflater)
+            setContentView(binding.root)
+
+            binding.textButtonDeleteAccount.setOnClickListener{
+                val user = auth.currentUser
+                user?.delete()
+                    ?.addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            Toast.makeText(this, "Account deleted", Toast.LENGTH_SHORT).show()
+                            val intent = Intent(this, MainActivity::class.java)
+                            startActivity(intent)
+                        }else{
+                            Toast.makeText(this, "Failed to delete account", Toast.LENGTH_SHORT).show()
+                            val exception = task.exception
+                            Log.e("DeleteAccount","Failed to delete account",exception)
+                        }
+                    }
+            }
+
+
+
+        }
+
+
     }
-}
