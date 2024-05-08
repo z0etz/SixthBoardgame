8ce9c26bad971338acefea45b10618d404b2fd15
jammingbadger <christofer2010@live.se>
2024-05-08 13:20:23 +0200

    Function display username

--- app/src/main/java/com/katja/sixthboardgame/Game.kt
+++ app/src/main/java/com/katja/sixthboardgame/Game.kt
@@ -1,12 +1,34 @@
 package com.katja.sixthboardgame
 
 import java.util.UUID
+import java.util.concurrent.CountDownLatch
 
-class Game(playerIdsList: List<String>) {
+class Game(private val userDao: UserDao, playerIdsList: List<String>) {
     var id = UUID.randomUUID().toString()
     val playerIds: List<String> = playerIdsList.shuffled()
     var nextPlayer: String = playerIds.first()
     var freeDiscsGray = 15
     var freeDiscsBrown = 15
     var gameboard: List<List<Stack<Stack.DiscColor>>> = List(5) { List(5) { Stack(mutableListOf()) } }
+
+    // Function to fetch usernames.
+    fun fetchUsernames(completion: (List<String>) -> Unit) {
+        val usernames = mutableListOf<String>()
+        val countDownLatch = CountDownLatch(playerIds.size)
+
+        for (playerId in playerIds) {
+            userDao.fetchUserById(playerId) { user ->
+                user?.let {
+                    usernames.add(user.userName)
+                }
+                countDownLatch.countDown()
+
+            }
+            
+        }
+        countDownLatch.await()
+        completion(usernames)
+
+
+    }
 }
