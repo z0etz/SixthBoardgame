e2a0250b5e3d50cd84315754e83f43684101fb79
Ali Alhasan <ali.alhassan8181@gmail.com>
2024-05-13 12:43:35 +0200

    Solving conflicts

--- app/src/main/java/com/katja/sixthboardgame/GameBoard.kt
+++ app/src/main/java/com/katja/sixthboardgame/GameBoard.kt
@@ -2,7 +2,7 @@ package com.katja.sixthboardgame
 
 class GameBoard {
 
-    var matrix: List<List<DiscStack<>>> = List(5) { List(5) { DiscStack(mutableListOf()) } }
+    var matrix: List<List<DiscStack>> = List(5) { List(5) { DiscStack(mutableListOf()) } }
 
 }
 
