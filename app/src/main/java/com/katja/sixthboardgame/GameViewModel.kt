317c4ed74243ae9bb93567fcbc3d10d74c4ed5c7
z0etz <146190783+z0etz@users.noreply.github.com>
2024-05-13 18:19:24 +0200

    Revert "Game dao 3"

--- app/src/main/java/com/katja/sixthboardgame/GameViewModel.kt
+++ app/src/main/java/com/katja/sixthboardgame/GameViewModel.kt
@@ -6,57 +6,50 @@ class GameViewModel {
 
     val gameDao = GameDao()
     var game: Game = Game(UserDao(),mutableListOf("1", "2", "3"))
+    fun loadGame(currentId: String?, opponentId: String?, gameId: String): Game {
+        gameDao.fetchGameById(currentId, opponentId, gameId) { game: Game ->
 
-    fun createNewGame(playerIdsList: List<String>): Game {
-        val game = Game(UserDao(), playerIdsList)
-        gameDao.addGame(game)
+            this.game = game
+        }
 
-        return game
+        return this.game
+
+        /*//TODO: Load existing game from Firebase, change return below to return that game
+        return Game(listOf("1","2"))
+
+         */
     }
 
-    fun loadUserOpponentGames(currentId: String?, opponentId: String?): MutableList<Game> {
-        var gameList = mutableListOf<Game>()
+    fun createNewGame(playerIdsList: List<String>): Game {
+        return Game(UserDao(), playerIdsList)
+    }
 
+
+    fun getGameById(
+        currentId: String?,
+        opponentId: String?,
+        gameId: String?,
+        callback: (Game) -> Unit
+    ) {
+        gameDao.fetchGameById(currentId, opponentId, gameId) { game: Game ->
+
+            callback(game)
+        }
+    }
+
+    fun saveGame(game: Game) {
+
+        gameDao.addGame(game)
+    }
+
+    fun loadGame2(currentId: String? , opponentId: String?): Game {
+        var gameList = mutableListOf<Game>()
         gameDao.fetchGamesAgainstOpponent(currentId, opponentId) {
 
             gameList = it
         }
 
 
-        return gameList
-    }
-
-
-
-    fun loadAllUserGames(currentId: String?): MutableList<Game> {
-        var gameList = mutableListOf<Game>()
-
-        gameDao.fetchAllUserGmes(currentId) {
-
-            gameList = it
-        }
-
-
-        return gameList
-    }
-
-
-    fun getGameById(gameId: String?): Game{
-
-        var game = Game(UserDao(), listOf("1", "2"))
-
-        gameDao.fetchGameById(gameId) { fetchedGame ->
-
-            game = fetchedGame
-        }
-
-        //TODO: Load existing game from Firebase, change return below to return that game
-
-        return game
-    }
-
-    fun saveGame(game: Game) {
-
-        gameDao.addGame(game)
+        return gameList.get(0)
     }
 }
