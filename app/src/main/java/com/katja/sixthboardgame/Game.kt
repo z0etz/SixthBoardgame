66ab253faa9f43bb901fd7cfda85e7c29fdc7a47
Ali Alhasan <ali.alhassan8181@gmail.com>
2024-05-14 11:47:23 +0200

    .

--- app/src/main/java/com/katja/sixthboardgame/Game.kt
+++ app/src/main/java/com/katja/sixthboardgame/Game.kt
@@ -9,7 +9,7 @@ class Game(userDao: UserDao, playerIdsList: List<String>) {
     var nextPlayer: String = playerIds.first()
     var freeDiscsGray = 15
     var freeDiscsBrown = 15
-    var gameboard: List<List<DiscStack>> = List(5) { List(5) { DiscStack(mutableListOf()) } }
+    var gameboard = GameBoard()
 
 //    constructor(userDao: UserDao, gameId: String, playerIdsList: List<String>) : this(userDao, playerIdsList) {
 //        this.id = gameId
