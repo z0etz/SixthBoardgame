745c0468293c76d5ad963d4f19b9f2b8f0dcb842
Leonardo <Leonardo@hotmail.com>
2024-05-23 11:29:07 +0200

    Legendary fix, PendingInvitesList filters out senderId.

--- app/src/main/java/com/katja/sixthboardgame/PendingSentInviteAdapter.kt
+++ app/src/main/java/com/katja/sixthboardgame/PendingSentInviteAdapter.kt
@@ -1,11 +1,14 @@
 package com.katja.sixthboardgame
 
+import android.content.ContentValues.TAG
 import android.content.Context
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 
 class PendingSentInviteAdapter(
@@ -13,6 +16,12 @@ class PendingSentInviteAdapter(
     private val inviteList: MutableList<String>,
     private val onDeleteClickListener: (Int) -> Unit
 ) : RecyclerView.Adapter<PendingSentInviteAdapter.InviteViewHolder>() {
+
+    private var auth: FirebaseAuth = FirebaseAuth.getInstance()
+
+    init {
+        filterSenderInvite()
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): InviteViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.pending_invites, parent, false)
@@ -52,11 +61,29 @@ class PendingSentInviteAdapter(
         notifyDataSetChanged()
     }
 
-    fun filterSenderInvite(){
-        val inviteCollection =
-        FirebaseFirestore
-            .getInstance()
-            .collection("Games")
+    private fun filterSenderInvite() {
+        val currentUserId = auth.currentUser?.uid
+
+        if (currentUserId != null) {
+            val inviteCollection = FirebaseFirestore
+                .getInstance()
+                .collection("game_invitations")
+
+            inviteCollection
+                .whereEqualTo("senderId", currentUserId)
+                .get()
+                .addOnSuccessListener { querySnapshot ->
+                    val filteredInvites = querySnapshot.documents.map { it.getString("receiverId") ?: "" }
+                    updateInvitationsList(filteredInvites)
+                }
+                .addOnFailureListener { e ->
+                    // Handle any errors that occur during the query
+                    Log.e("PendingSentInviteAdapter", "Error fetching invites", e)
+                }
+        }
     }
 
+    fun refreshList() {
+        filterSenderInvite()
+    }
 }
