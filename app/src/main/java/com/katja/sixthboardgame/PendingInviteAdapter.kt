4a14d04d4f714425117ea3fa19e4c64c93595587
luma <lucas.martinahlm@gmail.com>
2024-05-23 11:27:48 +0200

    bugg fixed no self inviting allowed

--- app/src/main/java/com/katja/sixthboardgame/PendingInviteAdapter.kt
+++ app/src/main/java/com/katja/sixthboardgame/PendingInviteAdapter.kt
@@ -4,28 +4,27 @@ package com.katja.sixthboardgame
 import android.app.Dialog
 import android.content.Context
 import android.content.Intent
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.Window
-import android.widget.AdapterView.OnItemClickListener
-import android.widget.AdapterView.inflate
 import android.widget.TextView
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.auth.FirebaseAuth
-import kotlinx.coroutines.withContext
+import kotlin.reflect.KFunction2
 
-    class PendingInviteAdapter(
-        private val context: Context,
-        private val inviteList: MutableList<String>,
-        private val receiverId: String,
-        private val onDeleteClickListener: (Int) -> Unit
-    ) : RecyclerView.Adapter<PendingInviteAdapter.InviteViewHolder>() {
+class PendingInviteAdapter(
+    private val context: Context,
+    private val inviteList: MutableList<String>,
+    private val receiverId: String,
+    private val onDeleteClickListener: (Int) -> Unit,
+    private val deleteInvite: KFunction2<String, String, Unit>
+) : RecyclerView.Adapter<PendingInviteAdapter.InviteViewHolder>() {
 
 
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): InviteViewHolder {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): InviteViewHolder {
             val view = LayoutInflater.from(parent.context).inflate(R.layout.pending_invites, parent, false)
             return InviteViewHolder(view)
         }
@@ -78,30 +77,35 @@ import kotlinx.coroutines.withContext
             val currentUser = FirebaseAuth.getInstance().currentUser
             val currentUserId = currentUser?.uid
 
-        if (currentUserId != null) {
-            val dialog = Dialog(context)
-            dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
-            dialog.setCancelable(false)
-            dialog.setContentView(R.layout.activity_game_dialog)
+            if (currentUserId != null) {
+                val dialog = Dialog(context)
+                dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+                dialog.setCancelable(false)
+                dialog.setContentView(R.layout.activity_game_dialog)
 
                 val buttonContinue = dialog.findViewById<TextView>(R.id.textButtonContinue)
                 val buttonCancel = dialog.findViewById<TextView>(R.id.textButtonCancel)
 
-            buttonContinue.setOnClickListener {
-                // Create a new a new game and add it to Firestore
-                GameDao().addGame(currentUserId, receiverId)
-                // Close dialog
-                dialog.dismiss()
-            }
+                buttonContinue.setOnClickListener {
 
-            buttonCancel.setOnClickListener {
-                dialog.dismiss()
-            }
+                    GameDao().addGame(currentUserId, receiverId)
 
-            dialog.show()
-        } else {
-            // Handle case when current user ID is null
-            Toast.makeText(context, "Current user ID is null", Toast.LENGTH_SHORT).show()
+                    dialog.dismiss()
+                }
+
+                buttonCancel.setOnClickListener {
+
+                    deleteInvite(senderId,receiverId)
+
+                    dialog.dismiss()
+                }
+
+                dialog.show()
+            } else {
+
+                Toast.makeText(context, "Current user ID is null", Toast.LENGTH_SHORT).show()
+            }
         }
+
     }
-}
+
