3bf77ab8d6f4e82200a8ba874420f7e6e84b1a8e
luma <lucas.martinahlm@gmail.com>
2024-05-27 12:05:09 +0200

    bugg fixed no self inviting allowed

--- app/src/main/java/com/katja/sixthboardgame/GameViewModel.kt
+++ app/src/main/java/com/katja/sixthboardgame/GameViewModel.kt
@@ -5,6 +5,7 @@ import com.google.firebase.firestore.FirebaseFirestore
 import javax.security.auth.callback.Callback
 
 class GameViewModel {
+    //hej
 
     val gameDao = GameDao()
     val userDao = UserDao()
